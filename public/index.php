070346fc746329aa8cb09d1ebc2cec9a1c99d7cf
butaosuinu <buta.osuinu@gmail.com>
2019-10-07 20:23:11 +0900

    add routing

--- public/index.php
+++ public/index.php
@@ -1,8 +1,15 @@
 <?php
+
 require __DIR__ . '/../vendor/autoload.php';
 
-echo "<!DOCTYPE html>\n";
-echo "<title>test</title>\n";
-echo "<p>" . h('hello new my flamework') . "</p>\n";
-echo "<p>現在は" . h(date('Y年m月d日H時i分s秒')) . "です</p>\n";
-echo "<ul><li><a href='/phpinfo.php'><code>phpinfo()</code></a></ul>\n";
+$routes = require __DIR__ . '/../app/routes.php';
+
+$request_uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+if (isset($routes[$request_uri])) {
+    $f = $routes[$request_uri];
+    $f();
+} else {
+    http_response_code(404);
+    echo "<h1>404 Not found</h1>";
+}
