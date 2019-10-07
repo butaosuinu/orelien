98c862035dd7e06d253044a92c1d8eea259ec811
butaosuinu <buta.osuinu@gmail.com>
2019-10-07 20:36:03 +0900

    add index.phtml

--- app/routes.php
+++ app/routes.php
@@ -2,12 +2,7 @@
 $routes = [];
 
 $routes['/'] = function () {
-    echo "<!DOCTYPE html>\n";
-    echo "<title>test</title>\n";
-    echo "<p>" . h('hello new my flamework') . "</p>\n";
-    echo "<p>現在は" . h(date('Y年m月d日H時i分s秒')) . "です</p>\n";
-    echo "<ul><li><a href='/phpinfo.php'><code>phpinfo()</code></a></ul>\n";
-    exit();
+    include __DIR__ . '/../app/view/index.phtml';
 };
 
 $routes['/phpinfo.php'] = function () {
