ee0a9594f252eaeaf63d9f44cbd616f9045b9730
butaosuinu <buta.osuinu@gmail.com>
2019-10-07 21:37:08 +0900

    update app

--- app/routes.php
+++ app/routes.php
@@ -1,10 +1,13 @@
 <?php
 $routes = [];
+$template = new \Orelien\TemplateFactory(__DIR__ . '/view/');
 
-$routes['/'] = function (): array {
-    ob_start();
-    include __DIR__ . '/../app/view/index.phtml';
-    return [200, ['Content-Type' => 'text/html'], ob_get_clean()];
+$routes['/'] = function () use ($template): array {
+    return [
+        200,
+        ['Content-Type' => 'text/html'],
+        $template->create('index', ['name' => 'ぶた'])
+    ];
 };
 
 $routes['/phpinfo.php'] = function (): void {
