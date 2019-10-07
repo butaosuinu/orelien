47962d9355756de6b9ba82dd1a5b9916ed98276d
butaosuinu <buta.osuinu@gmail.com>
2019-10-07 20:47:25 +0900

    change : return http status code & headers

--- app/routes.php
+++ app/routes.php
@@ -1,11 +1,14 @@
 <?php
 $routes = [];
 
-$routes['/'] = function () {
+$routes['/'] = function (): array {
+    ob_start();
     include __DIR__ . '/../app/view/index.phtml';
+    return [200, ['Content-Type' => 'text/html'], ob_get_clean()];
 };
 
-$routes['/phpinfo.php'] = function () {
+$routes['/phpinfo.php'] = function (): void {
+    ob_start();
     phpinfo();
 };
 
--- public/index.php
+++ public/index.php
@@ -8,7 +8,13 @@ $request_uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
 if (isset($routes[$request_uri])) {
     $f = $routes[$request_uri];
-    $f();
+
+    [$status, $headers, $body] = f();
+    http_response_code($status);
+    foreach ($header as $name => $h) {
+        header("{$name}: {$h}");
+    }
+    echo $body;
 } else {
     http_response_code(404);
     echo "<h1>404 Not found</h1>";
